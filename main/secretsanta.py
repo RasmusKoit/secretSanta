5a0feb4bd3c6054cf6a213f210cdb8120b871cb4
Rasmus Koit <rasmus@Rasmus.kontor>
2017-12-11 17:06:28 +0200

    Secret Santa generator
    Matches a list of names with a random choice.
    
    Writes to a file and also holds it in a variable called finalList
    
    Signed-off-by: Rasmus Koit <rasmuskoit@gmail.com>

--- main/secretsanta.py
+++ main/secretsanta.py
@@ -1,4 +1,38 @@
-def
+def readFileToList(filename):
+    list = []
+    with open(filename, "r") as file:
+        for line in file:
+            list.append(line.rstrip())
+        file.close()
+    return list
+
+
+def generateNames(mainlist):
+    from random import choice
+    finalList = []
+    tempList = mainlist[:]
+    check = True
+    writeToFile("[Teeb kingi]: [Saab kingi]")
+    for name in mainlist:
+        while check:
+            SSName = choice(tempList)
+            if name != SSName:
+                string = name + ": " + SSName
+                finalList.append(string)
+                writeToFile(string)
+                check = False
+                tempList.remove(SSName)
+        check = True
+    return finalList
+
+
+def writeToFile(string):
+    with open("secretSanta.txt", "a") as f:
+        f.write(string + '\n')
+    f.close()
 
 
 if __name__ == '__main__':
+    nameList = readFileToList("nimed.txt")
+    print(nameList)
+    SSList = generateNames(nameList)
